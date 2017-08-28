9705ecd339e416172ab19e184508c53927e63348
Chakrit Prasatwattana <chakritp888@gmail.com>
2017-08-28 16:51:27 -0700

    Add css to rotate pokeballs on hover

--- js/main.js
+++ js/main.js
@@ -38,22 +38,43 @@ var pokemon2 = {
   name: 'Ho Oh',
   health: 3000,
   avatar: {
-    front: '',
-    back: ''
+    front: 'http://www.pokestadium.com/sprites/xy/ho-oh.gif',
+    back: 'http://www.pokestadium.com/sprites/xy/back/ho-oh.gif'
   },
   attack: 240,
   defence: 500,
   speed: 800,
   specialAttack: 4000,
   specialDefence: 2000,
-  moves: ['fly', 'fireblast', 'rainbow blast', 'peck']
+  moves: [
+    {
+      name: 'Fly',
+      damage: 100,
+      element: 'flying'
+    },
+    {
+      name: 'Fireblast',
+      damage: 100,
+      element: 'fire'
+    },
+    {
+      name: 'Rainbow Blast',
+      damage: 100,
+      element: 'fire'
+    },
+    {
+      name: 'Peck',
+      damage: 100,
+      element: 'flying'
+    }
+  ]
 }
 
 var pokemon3 = {
   name: 'Lugia',
   avatar: {
-    front: '',
-    back: ''
+    front: 'http://www.pokestadium.com/sprites/xy/lugia.gif',
+    back: 'http://www.pokestadium.com/sprites/xy/back/lugia.gif'
   },
   health: 3500,
   attack: 3000,
@@ -61,32 +82,219 @@ var pokemon3 = {
   speed: 900,
   specialAttack: 3000,
   specialDefence: 4000,
-  moves: ['hydropump', 'hyper beam', 'surf', 'psychic']
+  moves: [
+    {
+      name: 'Hydropump',
+      damage: 100,
+      element: 'water'
+    },
+    {
+      name: 'Hyper Beam',
+      damage: 100,
+      element: 'normal'
+    },
+    {
+      name: 'Surf',
+      damage: 100,
+      element: 'water'
+    },
+    {
+      name: 'Psychic',
+      damage: 100,
+      element: 'psychic'
+    }
+  ]
 }
 
 var pokemon4 = {
   name: 'Kyogre',
+  avatar: {
+    front: 'http://www.pokestadium.com/sprites/xy/kyogre.gif',
+    back: 'http://www.pokestadium.com/sprites/xy/back/kyogre.gif'
+  },
   health: 3500,
   attack: 3000,
   defence: 700,
   speed: 900,
   specialAttack: 3000,
   specialDefence: 4000,
-  moves: ['hydropump', 'water fall', 'surf', 'dive']
+  moves: [
+    {
+      name: 'Hydropump',
+      damage: 100,
+      element: 'water'
+    },
+    {
+      name: 'Waterfall',
+      damage: 100,
+      element: 'water'
+    },
+    {
+      name: 'Surf',
+      damage: 100,
+      element: 'water'
+    },
+    {
+      name: 'Dive',
+      damage: 100,
+      element: 'water'
+    }
+  ]
 }
 
 var pokemon5 = {
   name: 'Groudon',
+  avatar: {
+    front: 'http://www.pokestadium.com/sprites/xy/groudon.gif',
+    back: 'http://www.pokestadium.com/sprites/xy/back/groudon.gif'
+  },
   health: 3500,
   attack: 3000,
   defence: 700,
   speed: 900,
   specialAttack: 3000,
   specialDefence: 4000,
-  moves: ['earthquake', 'hyper beam', 'fire blast', 'blaze']
+  moves: [
+    {
+      name: 'Earthquake',
+      damage: 100,
+      element: 'ground'
+    },
+    {
+      name: 'Hyper Beam',
+      damage: 100,
+      element: 'normal'
+    },
+    {
+      name: 'Fire Blast',
+      damage: 100,
+      element: 'fire'
+    },
+    {
+      name: 'Blaze',
+      damage: 100,
+      element: 'fire'
+    }
+  ]
 }
 
 var pokemon6 = {
+  name: 'Articuno',
+  avatar: {
+    front: 'http://www.pokestadium.com/sprites/xy/articuno.gif',
+    back: 'http://www.pokestadium.com/sprites/xy/back/articuno.gif'
+  },
+  health: 3500,
+  attack: 3000,
+  defence: 700,
+  speed: 900,
+  specialAttack: 3000,
+  specialDefence: 4000,
+  moves: [
+    {
+      name: 'Ice Beam',
+      damage: 100,
+      element: 'ice'
+    },
+    {
+      name: 'Hyper Beam',
+      damage: 100,
+      element: 'normal'
+    },
+    {
+      name: 'Fly',
+      damage: 100,
+      element: 'flying'
+    },
+    {
+      name: 'Blizzard',
+      damage: 100,
+      element: 'ice'
+    }
+  ]
+}
+
+var pokemonA = {
+  name: 'Blastoise',
+  avatar: {
+    front: 'http://www.pokestadium.com/sprites/xy/charizard-megay.gif',
+    back: 'http://www.pokestadium.com/sprites/xy/back/charizard.gif'
+  },
+  health: 3500,
+  attack: 3000,
+  defence: 700,
+  speed: 900,
+  specialAttack: 3000,
+  specialDefence: 4000,
+  moves: [
+    {
+      name: "Ice Beam",
+      element: "ice",
+      damage: 100
+    },
+    {
+      name: "Hyper Beam",
+      element: "normal",
+      damage: 100
+    },
+    {
+      name: "Fly",
+      element: "flying",
+      damage: 100
+    },
+    {
+      name: "Blizzard",
+      element: "ice",
+      damage: 100
+    }
+  ]
+}
+
+var pokemonB = {
+  name: 'Articuno',
+  health: 3500,
+  attack: 3000,
+  defence: 700,
+  speed: 900,
+  specialAttack: 3000,
+  specialDefence: 4000,
+  moves: ['ice beam', 'hyper beam', 'fly', 'blizzard']
+}
+
+var pokemonC = {
+  name: 'Articuno',
+  health: 3500,
+  attack: 3000,
+  defence: 700,
+  speed: 900,
+  specialAttack: 3000,
+  specialDefence: 4000,
+  moves: ['ice beam', 'hyper beam', 'fly', 'blizzard']
+}
+
+var pokemonD = {
+  name: 'Articuno',
+  health: 3500,
+  attack: 3000,
+  defence: 700,
+  speed: 900,
+  specialAttack: 3000,
+  specialDefence: 4000,
+  moves: ['ice beam', 'hyper beam', 'fly', 'blizzard']
+}
+
+var pokemonE = {
+  name: 'Articuno',
+  health: 3500,
+  attack: 3000,
+  defence: 700,
+  speed: 900,
+  specialAttack: 3000,
+  specialDefence: 4000,
+  moves: ['ice beam', 'hyper beam', 'fly', 'blizzard']
+}
+
+var pokemonF = {
   name: 'Articuno',
   health: 3500,
   attack: 3000,
@@ -104,7 +312,11 @@ var ash = {
 
 var gary = {
   name: "Gary",
-  pokemon: []
+  pokemon: [pokemonA, pokemonB, pokemonC, pokemonD, pokemonE, pokemonF]
+}
+
+var rotatePokeball = function(){
+  this.classList.toggle('rotate');
 }
 
 var game = {
@@ -112,17 +324,28 @@ var game = {
   player2: gary,
   start: function(){
     // Name
-    document.querySelector('#player-1 .name').innerText = this.player1.pokemon[0].name;
-    // document.querySelector('#player-2 .name').innerText = this.player2.pokemon[0].name;
+    p1FirstPokemon = this.player1.pokemon[0];
+    p2FirstPokemon = this.player2.pokemon[0];
+
+    document.querySelector('#player-1 .name').innerText = p1FirstPokemon.name;
+    document.querySelector('#player-2 .name').innerText = p2FirstPokemon.name;
 
     //Avatar
-    document.querySelector('#player-1 .avatar img').setAttribute('src', this.player1.pokemon[0].avatar.back);
-    // document.querySelector('#player-1 .avatar img').setAttribute('src', this.player2.pokemon[0].avatar.back);
+    document.querySelector('#player-1 .avatar img').setAttribute('src', p1FirstPokemon.avatar.back);
+    // document.querySelector('#player-2 .avatar img').setAttribute('src', p2FirstPokemon.avatar.front);
 
     //Moves
-    for(var i = 0; i < this.player1.pokemon[0].moves.length; i++){
-      document.querySelector('#player-1 #move-' + (i+1)).innerText = this.player1.pokemon[0].moves[i].name;
-      document.querySelector('#player-1 #move-' + (i+1)).setAttribute('class', this.player1.pokemon[0].moves[i].element);
+    for(var i = 0; i < p1FirstPokemon.moves.length; i++){
+      document.querySelector('#player-1 #move-' + (i+1)).innerText = p1FirstPokemon.moves[i].name;
+      document.querySelector('#player-1 #move-' + (i+1)).setAttribute('class', p1FirstPokemon.moves[i].element);
+    }
+
+    //Set party pokemon
+    var partyPokemon = document.querySelectorAll('#player-1 .party');
+    for(var i = 0; i < partyPokemon.length; i++){
+      // Set listener to rotate and un-rotate
+      partyPokemon[i].querySelector('img').addEventListener('mouseenter', rotatePokeball);
+      partyPokemon[i].querySelector('img').addEventListener('mouseout', rotatePokeball);
     }
   }
 };
