a4e48628a66f299d5a5b1a7f8ea6a5407410a270
Chakrit Prasatwattana <chakritp888@gmail.com>
2017-09-06 14:01:30 -0700

    Refactor player board setup in start method

--- js/main.js
+++ js/main.js
@@ -236,52 +236,11 @@ $(function() {
       checkOpponentFainted(player, opponent, $opponentBox, game)
     },
     start: function(){
-      // Name
       p1FirstPokemon = this.player1.pokemon[0];
       p2FirstPokemon = this.player2.pokemon[0];
-
-      $playerOneBox.find('.name').text(p1FirstPokemon.name)
-      $playerTwoBox.find('.name').text(p2FirstPokemon.name)
-
-      // Avatar
-      $playerOneBox.find('.avatar img').prop('src', p1FirstPokemon.avatar.back)
-      $playerTwoBox.find('.avatar img').prop('src', p2FirstPokemon.avatar.front)
-
-      // Health
-      $playerOneBox.find('.health .remaining').text(p1FirstPokemon.stats.health)
-      $playerOneBox.find('.health .max').text(p1FirstPokemon.stats.health)
-      $playerTwoBox.find('.health .remaining').text(p2FirstPokemon.stats.health)
-      $playerTwoBox.find('.health .max').text(p2FirstPokemon.stats.health)
-
-      // Moves
-      var $movesPlayerOne = $playerOneBox.find('.moves li')
-      $movesPlayerOne.each(function(index, move){
-        $(move).addClass(p1FirstPokemon.moves[index].element)
-        $(move).text(p1FirstPokemon.moves[index].name)
-      })
-
-      var $movesPlayerTwo = $playerTwoBox.find('.moves li')
-      $movesPlayerTwo.each(function(index, move){
-        $(move).addClass(p2FirstPokemon.moves[index].element)
-        $(move).text(p2FirstPokemon.moves[index].name)
-      })
-
-      // Party Pokemon
-      var $partyPokemonP1 = $playerOneBox.find('.party')
-      var $partyPokemonP2 = $playerTwoBox.find('.party')
-
-      // set tooltip text
-      $partyPokemonP1.each(function(index, partyPokemon) {
-        var pokemonName = game.player1.pokemon[index + 1].name
-        $(partyPokemon).find('.tooltip').text(pokemonName)
-        $(partyPokemon).find('img').prop('id', pokemonName)
-      })
-
-      $partyPokemonP2.each(function(index, partyPokemon) {
-        var pokemonName = game.player2.pokemon[index + 1].name
-        $(partyPokemon).find('.tooltip').text(pokemonName)
-        $(partyPokemon).find('img').prop('id', pokemonName)
-      })
+      
+      setUpPlayerBoard($playerOneBox, p1FirstPokemon, this)
+      setUpPlayerBoard($playerTwoBox, p2FirstPokemon, this)
 
       //set listener to show/hide tooltip
       $('.party-pokemon').on('mouseenter mouseout', 'img', function() {
